af129faf90bf2cc83a44696705887b7a58de6a54
placidowang <placidow@gmail.com>
2020-02-28 12:06:53 -0600

    built everything

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,2 +1,9 @@
 class Episode < ApplicationRecord
+  has_many :appearances
+  has_many :guests, through: :appearances
+
+  def average_rating
+    total = self.appearances.reduce(0) {|sum, appearance| sum + appearance.rating }.to_f
+    (total / self.appearances.count).round(1)
+  end
 end
