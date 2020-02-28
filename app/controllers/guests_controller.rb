8a52470e83b5a74741864154f737d8b01d84b0b8
Gavrilajava <georgy.gavrilchik@gmail.com>
2020-02-28 12:00:56 -0600

    all done

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -3,4 +3,11 @@ class GuestsController < ApplicationController
   def index
     @guests = Guest.all
   end
+
+  def show
+    @guest = Guest.find(params[:id])
+    @episodes = @guest.episodes.sort { |a,b|
+      b.rating_of(@guest) <=> a.rating_of(@guest)
+    }
+  end
 end
